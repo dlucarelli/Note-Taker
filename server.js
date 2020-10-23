d36aac7f759e96bb3bdc9f440514ffcb3b98fc91
Dylan Lucarelli <dyllucar@gmail.com>
2020-10-23 18:34:24 -0500

    Updates delete functionality and display

--- server.js
+++ server.js
@@ -12,42 +12,54 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"))
-});
-
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"))
 });
 
 app.get("/api/notes", function(req, res) {
-    fs.readFile("./db/db.json", function(err, data) {
-        if(err) {
-            throw err
-        }
-        return res.json(JSON.parse(data))
-    });
-})
-
-app.post("/api/notes", function(req, res) {
-    notesArray.push(req.body);
-    fs.writeFile("./db/db.json", JSON.stringify(notesArray), function(err) {
-        if(err) {
-            throw err
-        };
-        return res.json(req.body);
-    });
-    console.log(notesArray);
-
+    res.sendFile(path.join(__dirname, "./db/db.json")
+    )
 });
 
-// app.delete()
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+});
 
+app.post("/api/notes", function(req, res) {
+    let savedNote = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let newNote = req.body;
+    let noteId = (savedNote.length).toString();
+    newNote.id = noteId;
+    savedNote.push(newNote);
 
-// /api/notes/:id
-//req.params.id
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNote));
+    res.json(savedNote);
+});
 
+app.get("/api/notes/:id", function(req, res) {
+    let savedNote = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    res.json(savedNote[Number(req.params.id)]); 
+});
 
-app.listen(PORT, function () {
+app.delete("/api/notes/:id", function(req, res) {
+    let savedNote = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteID = req.params.id;
+    let newID = 0;
+    console.log(`Deleting note with ID ${noteID}`);
+    savedNote = savedNote.filter(currNote => {
+        return currNote.id != noteID;
+    })
+    
+    for (currNote of savedNote) {
+        currNote.id = newID.toString();
+        newID++;
+    }
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNote));
+    res.json(savedNote);
+});
+
+app.listen(PORT, function (err) {
+    if(err) console.log(err);
     console.log("Successfully running on Port: " + PORT)
-})
+});
